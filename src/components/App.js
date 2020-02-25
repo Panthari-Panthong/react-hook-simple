53015718d2302206f6c799488a39cf85a380feff
P Panthong <panthari.panthong@gmail.com>
2020-02-25 18:17:21 +0100

    building the resource list

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
 
-class App extends Component {
-  state = {
-    resource : 'posts'
-  }
+const App = () => {
+  const [resource, setResource] = useState('posts');
+  // 
 
-  render() {
     return (
       <div>
         <div>
-          <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
-          <button onClick={() => this.setState({resource: 'todos'})}>Todos</button>
+          <button onClick={() => setResource("posts")}>Posts</button>
+          <button onClick={() => setResource("todos")}>Todos</button>
         </div>
-        {this.state.resource}
+        <ResourceList resource={resource} />
       </div>
     );
-  }
 }
 
 export default App;
